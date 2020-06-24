28337b003553450b435af577b9c3bd22e856f76b
Martin Nguyen <martin.nguyen.20289@gmail.com>
2020-06-24 18:14:41 -0400

    fixed bugs and added functionality

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
-const dayInHistory = {}
+const scroll = {}
+scroll.leftScroll = document.getElementsByClassName('left-scroll')[0];
+scroll.rightScroll = document.getElementsByClassName('right-scroll')[0];
+scroll.scrollBody = document.getElementsByClassName('scroll-body')[0];
 
+const dayInHistory = {}
 dayInHistory.host = 'http://history.muffinlabs.com';
 dayInHistory.display = document.getElementsByClassName('display')[0];
 dayInHistory.slider = document.getElementsByClassName('time-slider')[0];
 dayInHistory.progressBar = document.getElementsByClassName('progress-bar')[0];
-
 dayInHistory.getToday = function() {
     const today = new Date();
     let month = today.getMonth() + 1;
     let day = today.getDate();
     return [month, day];
 }
-
 dayInHistory.getHistory = async function() {
     const date = this.getToday();
     try {
@@ -25,41 +27,39 @@ dayInHistory.getHistory = async function() {
         console.log(err);
     }
 }
-
 dayInHistory.getEvent = function(events) {
     let random = 0;
     events.forEach(event => {
-        // image
-        const imageContainer = document.createElement('div');
-        const image = document.createElement('img');
-        imageContainer.classList.add('image-container');
-        random = this.randomImageIndex(random);
-        const era = this.getEra(event.year, event.text, random);
-        image.src = era[0];
-        image.setAttribute('id', era[1]);
-        imageContainer.appendChild(image);
-        
-        // text description of historical events
-        const textContainer = document.createElement('div');
-        const yearText = document.createElement('h2');
-        const description = document.createElement('p');
-        yearText.classList.add('content');
-        description.classList.add('content');
-        yearText.innerHTML = `Year ${ event.year }`;
-        description.innerHTML = event.text;
-        textContainer.classList.add('text-container');
-        textContainer.innerHTML = yearText.outerHTML + description.outerHTML;
-
-        // content container
         const eventDisplay = document.createElement('div');
         eventDisplay.classList.add('content-container')
-        eventDisplay.innerHTML = imageContainer.outerHTML + textContainer.outerHTML;
+        eventDisplay.innerHTML = this.getImage(event, random).outerHTML + this.getDescription(event).outerHTML;
         eventDisplay.classList.add('hidden')
-
         this.display.appendChild(eventDisplay);
     })
 }
-
+dayInHistory.getImage = function(event, random) {
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    imageContainer.classList.add('image-container');
+    random = this.randomImageIndex(random);
+    const era = this.getEra(event.year, event.text, random);
+    image.src = era[0];
+    image.setAttribute('id', era[1]);
+    imageContainer.appendChild(image);
+    return imageContainer;
+}
+dayInHistory.getDescription = function(event) {
+    const textContainer = document.createElement('div');
+    const yearText = document.createElement('h2');
+    const description = document.createElement('p');
+    yearText.classList.add('content');
+    description.classList.add('content');
+    yearText.innerHTML = `Year ${ event.year }`;
+    description.innerHTML = event.text;
+    textContainer.classList.add('text-container');
+    textContainer.innerHTML = yearText.outerHTML + description.outerHTML;
+    return textContainer;
+}
 dayInHistory.randomImageIndex = function(prevNum) {
     let random = Math.floor(Math.random()*5 + 1);
     while (random === prevNum) {
@@ -67,31 +67,17 @@ dayInHistory.randomImageIndex = function(prevNum) {
     }
     return random;
 }
-
+dayInHistory.isWWI = function(text) {
+    return text.toLowerCase().includes('world war 1') || text.toLowerCase().includes('ww1') || text.toLowerCase().includes('world war i') || text.toLowerCase().includes('wwi');
+}
+dayInHistory.isWWII = function(text) {
+    return text.toLowerCase().includes('world war 2') || text.toLowerCase().includes('ww2') || text.toLowerCase().includes('world war ii') || text.toLowerCase().includes('wwii');
+}
 dayInHistory.getYearNumeral = function(year) { // convert 'BC' to negative
     let yearNumeral = parseInt(year);
     if (year.includes(' BC')) { yearNumeral = -yearNumeral }
     return yearNumeral;
 }
-
-dayInHistory.showEra = function(era) {
-    for (let i = 0; i < 5; i++) {
-        if (this.slider.children[i].classList[0] === era) {
-            this.slider.children[i].classList.remove('hidden');
-        } else { 
-            this.slider.children[i].classList.add('hidden');
-        }
-    }
-}
-
-dayInHistory.isWWI = function(text) {
-    return text.toLowerCase().includes('world war 1') || text.toLowerCase().includes('ww1') || text.toLowerCase().includes('world war i') || text.toLowerCase().includes('wwi');
-}
-
-dayInHistory.isWWII = function(text) {
-    return text.toLowerCase().includes('world war 2') || text.toLowerCase().includes('ww2') || text.toLowerCase().includes('world war ii') || text.toLowerCase().includes('wwii');
-}
-
 dayInHistory.getEra = function(year, text, random) {
     let yearNumeral = this.getYearNumeral(year);
     let src = 'assets/';
@@ -108,7 +94,6 @@ dayInHistory.getEra = function(year, text, random) {
         keyword = 'current';
     }
     src += `${ keyword + random }.jpg`;
-
     // attempts to make images content-aware
     if (this.isWWII(text)) {
         src = `assets/WW2.jpg`;
@@ -122,11 +107,18 @@ dayInHistory.getEra = function(year, text, random) {
     }
     return [src, keyword];
 }
+dayInHistory.showEra = function(era) {
+    for (let i = 0; i < 5; i++) {
+        if (this.slider.children[i].classList[0] === era) {
+            this.slider.children[i].classList.remove('hidden');
+        } else { 
+            this.slider.children[i].classList.add('hidden');
+        }
+    }
+}
 dayInHistory.init = function() {
     this.getHistory();
 }
-
-
 
 helperFunctions = {}
 helperFunctions.initialized = false; // boolean value to initialize functions only once 
@@ -144,121 +136,120 @@ helperFunctions.animateNav = function() { // toggle padding to create animation
         on = !on;
     }, 500);
 }
-helperFunctions.handleMouseScroll = function() {
+helperFunctions.handleMouseScroll = function(element) {
     const displayContent = dayInHistory.display.childNodes;
-    dayInHistory.display.addEventListener('wheel', function(e) {
-        if (e.deltaY > 0) { // scroll down
-            // prevent scrolling past 0
-            if (displayContent[helperFunctions.index - 1]) { displayContent[helperFunctions.index - 1].classList.add('hidden'); }
-            // after scrolling through all contents, start scroll closing process by reversing scroll opening
-            if (!displayContent[helperFunctions.index]) {
-                helperFunctions.close();
-            }
-            if (dayInHistory.display.classList.contains('revealed')) {
-                let era = displayContent[helperFunctions.index].childNodes[0].childNodes[0].getAttribute('id');
-                dayInHistory.showEra(era);
+    element.addEventListener('wheel', function(e) {
+        if (dayInHistory.display.classList.contains('ready')) {
+            if (e.deltaY > 0) { // scroll down
+                if (displayContent[helperFunctions.index - 1]) { displayContent[helperFunctions.index - 1].classList.add('hidden'); }
+                // after scrolling through all contents, start scroll closing process by reversing scroll opening
+                if (!displayContent[helperFunctions.index]) { helperFunctions.close(); } 
+                helperFunctions.fillProgressBar(displayContent);
                 displayContent[helperFunctions.index++].classList.remove('hidden');
-                dayInHistory.progressBar.style.width = `${ 50 * helperFunctions.index / displayContent.length }vw`;
-            }
-            
-        } else { //scroll up
-            if (displayContent[helperFunctions.index - 1]) { 
-                displayContent[helperFunctions.index--].classList.add('hidden');
-                let era = displayContent[helperFunctions.index].childNodes[0].childNodes[0].getAttribute('id');
-                dayInHistory.showEra(era);
-                dayInHistory.progressBar.style.width = `${ 50 * helperFunctions.index / displayContent.length }vw`;
-                displayContent[helperFunctions.index].classList.remove('hidden'); 
+            } else { //scroll up
+                if (displayContent[helperFunctions.index - 1]) { // prevent scrolling past 0
+                    displayContent[helperFunctions.index--].classList.add('hidden');
+                    helperFunctions.fillProgressBar(displayContent);
+                    displayContent[helperFunctions.index].classList.remove('hidden'); 
+                }
             }
         }
     });
 }
-
-helperFunctions.handleClick = function() {
+helperFunctions.fillProgressBar = function(content) {
+    let era = content[helperFunctions.index].childNodes[0].childNodes[0].getAttribute('id');
+    dayInHistory.showEra(era);
+    dayInHistory.progressBar.style.width = `${ 50 * helperFunctions.index / content.length }vw`;   
+}
+helperFunctions.handleClick = function(element) { 
+    element.addEventListener('click', function(e) {
+        helperFunctions.synchProgressBar(e);
+        helperFunctions.synchDisplayContent();
+    });
+}
+helperFunctions.synchProgressBar = function(e) {
     const displayContent = dayInHistory.display.childNodes;
-
-    dayInHistory.slider.addEventListener('click', function(e) {
-        const progressBarLength = 0.5 * window.innerWidth;
-        helperFunctions.index = Math.floor(displayContent.length * (e.clientX - dayInHistory.slider.offsetLeft) / progressBarLength);
-        dayInHistory.progressBar.style.width = `${ 50 * helperFunctions.index / displayContent.length }vw`;
-        for (let i = 0; i < displayContent.length; i++) {
-            if (i === helperFunctions.index) {
-                let era = displayContent[helperFunctions.index].childNodes[0].childNodes[0].getAttribute('id');
-                dayInHistory.showEra(era);
-                displayContent[i].classList.remove('hidden');
-            } else {
-                displayContent[i].classList.add('hidden');
-            }
-        }
-    });
-
-    dayInHistory.progressBar.addEventListener('click', function(e) {
-        const progressBarLength = 0.5 * window.innerWidth;
-        helperFunctions.index = Math.floor(displayContent.length * (e.clientX - dayInHistory.slider.offsetLeft) / progressBarLength);
-        dayInHistory.progressBar.style.width = `${ 50 * helperFunctions.index / displayContent.length }vw`;
-        for (let i = 0; i < displayContent.length; i++) {
-            if (i === helperFunctions.index) {
-                let era = displayContent[helperFunctions.index].childNodes[0].childNodes[0].getAttribute('id');
-                dayInHistory.showEra(era);
-                displayContent[i].classList.remove('hidden');
-            } else {
-                displayContent[i].classList.add('hidden');
-            }
-        }
-    });
-
-    if (this.index === dayInHistory.display.childNodes.length) { return; }
+    const progressBarLength = 0.5 * window.innerWidth;
+    helperFunctions.index = Math.floor(displayContent.length * (e.clientX - dayInHistory.slider.offsetLeft) / progressBarLength);
+    dayInHistory.progressBar.style.width = `${ 50 * helperFunctions.index / displayContent.length }vw`;
 }
-
+helperFunctions.synchDisplayContent = function() {
+    const displayContent = dayInHistory.display.childNodes;
+    for (let i = 0; i < displayContent.length; i++) {
+        if (i === helperFunctions.index) {
+            let era = displayContent[helperFunctions.index].childNodes[0].childNodes[0].getAttribute('id');
+            dayInHistory.showEra(era);
+            displayContent[i].classList.remove('hidden');
+        } else {
+            displayContent[i].classList.add('hidden');
+        }
+    }
+}
+helperFunctions.handleCloseButton = function() {
+    document.getElementsByClassName('close-button')[0].addEventListener('click', function() {
+        helperFunctions.close();
+    });
+}
 helperFunctions.open = function() {
     const overlayButton = document.getElementsByClassName('overlay')[0];
-    overlayButton.addEventListener('click', function() {
+    overlayButton.addEventListener('click', function() { // start scroll opening animation on overlay button being clicked
         helperFunctions.index = 0;
-        // start scroll opening animation on overlay button being clicked
-        document.getElementsByClassName('left-scroll')[0].classList.remove('close');
-        document.getElementsByClassName('right-scroll')[0].classList.remove('close');
-        document.getElementsByClassName('left-scroll')[0].classList.add('left-open');
-        document.getElementsByClassName('right-scroll')[0].classList.add('right-open');
-        document.getElementsByClassName('time-slider')[0].classList.add('revealed');
-        dayInHistory.display.childNodes.forEach(display => {
-            display.classList.add('hidden');
-        });
+        scroll.leftScroll.classList.remove('close');
+        scroll.leftScroll.classList.add('left-open');
+        scroll.rightScroll.classList.remove('close');
+        scroll.rightScroll.classList.add('right-open');
+        scroll.scrollBody.classList.remove('hidden');
+        dayInHistory.slider.classList.add('revealed');
         dayInHistory.display.classList.add('revealed');
         overlayButton.classList.add('hidden');
         setTimeout(function() {
+            document.getElementsByClassName('close-button')[0].classList.remove('hidden');
             document.getElementsByClassName('navigation-top')[0].classList.remove('hidden');
             document.getElementsByClassName('navigation-bottom')[0].classList.remove('hidden');
             dayInHistory.display.childNodes[0].classList.remove('hidden');
+            dayInHistory.display.classList.add('ready');
             dayInHistory.progressBar.classList.remove('hidden');
             dayInHistory.progressBar.style.width = 0;
             dayInHistory.showEra(dayInHistory.display.childNodes[0].childNodes[0].childNodes[0].getAttribute('id'));
-            
-            if (!helperFunctions.initialized) {
-                // only initialize these functions once
+            if (!helperFunctions.initialized) { // only initialize these functions once
                 helperFunctions.initialized = true;
-                helperFunctions.handleMouseScroll(helperFunctions.index);
-                helperFunctions.handleClick();
+                helperFunctions.handleMouseScroll(dayInHistory.display);
+                helperFunctions.handleMouseScroll(dayInHistory.slider);
+                helperFunctions.handleMouseScroll(dayInHistory.progressBar);
+                helperFunctions.handleClick(dayInHistory.slider);
+                helperFunctions.handleClick(dayInHistory.progressBar)
+                helperFunctions.animateNav();
+                helperFunctions.handleCloseButton();
             }
         }, 1000);
     });     
 }
 helperFunctions.close = function() {
-    document.getElementsByClassName('left-scroll')[0].classList.remove('left-open');
-    document.getElementsByClassName('left-scroll')[0].classList.add('close');
-    document.getElementsByClassName('right-scroll')[0].classList.remove('right-open');
-    document.getElementsByClassName('right-scroll')[0].classList.add('close');
+    scroll.leftScroll.classList.remove('left-open');
+    scroll.leftScroll.classList.add('close');
+    scroll.rightScroll.classList.remove('right-open');
+    scroll.rightScroll.classList.add('close');
+    dayInHistory.progressBar.classList.add('hidden');
+    dayInHistory.display.classList.remove('ready');
     document.getElementsByClassName('closing')[0].classList.add('revealed');
+    document.getElementsByClassName('close-button')[0].classList.add('hidden');
     document.getElementsByClassName('navigation-top')[0].classList.add('hidden');
     document.getElementsByClassName('navigation-bottom')[0].classList.add('hidden');
-    dayInHistory.progressBar.classList.add('hidden');
     setTimeout(function() {
-        document.getElementsByClassName('time-slider')[0].classList.remove('revealed');
-        dayInHistory.display.classList.remove('revealed');
+        scroll.scrollBody.classList.add('hidden');
+        dayInHistory.slider.classList.remove('revealed');
+        dayInHistory.display.classList.remove('revealed'); 
+    }, 750);
+    setTimeout(function() { 
         document.getElementsByClassName('overlay')[0].classList.remove('hidden');
+        for (let i = 0; i < 5; i++) { dayInHistory.slider.children[i].classList.add('hidden') }
+        dayInHistory.display.childNodes.forEach(display => { 
+            if (!display.classList.contains('hidden')) { display.classList.add('hidden'); }
+        });
     }, 1000);
 }
 helperFunctions.init = function() {
     this.open();
-    this.animateNav();
 }
 
 
